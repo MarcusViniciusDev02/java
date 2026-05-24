6afae5d2756f84b5eee9c87cd57b8c20bdc1757c
Marcus Vinicius Paixao do Nascimento <Marcus.Paixao@hgj.gov>
2026-05-24 16:48:58 -0300

    att

---
+++ ProjetoCadastro/src/main/java/dev/Projeto/ProjetoCadastro/Controller.java
@@ -0,0 +1,18 @@
+package dev.Projeto.ProjetoCadastro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Mapeia e fala pro java que aqui é um controller.
+@RequestMapping //Mapeia as requisições
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é a minha primeira mensagem nessa rota.";
+    }
+
+
+
+}
