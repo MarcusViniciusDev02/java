bfcb44b2d2be0846fea4aa96956a21f9a8ee8043
Marcus Vinicius Paixao do Nascimento <Marcus.Paixao@hgj.gov>
2026-05-24 18:48:34 -0300

    #2 issue - P67

--- ProjetoCadastro/src/main/java/dev/Projeto/ProjetoCadastro/NinjaController.java
+++ ProjetoCadastro/src/main/java/dev/Projeto/ProjetoCadastro/NinjaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController //Mapeia e fala pro java que aqui é um controller.
 @RequestMapping //Mapeia as requisições
-public class Controller {
+public class NinjaController {
 
     @GetMapping("/boasvindas")
     public String boasVindas(){
--- ProjetoCadastro/src/main/java/dev/Projeto/ProjetoCadastro/teste.java
+++
@@ -1,4 +0,0 @@
-package dev.Projeto.ProjetoCadastro;
-
-public class teste {
-}
